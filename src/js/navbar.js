7498f0fb7208d410672b94c08d56ae03019872b2
Christian Paling <christian.paling@googlemail.com>
2016-04-06 23:41:58 +0200

    added basic layout for menu and empty files for pages

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,16 +1,22 @@
 (function() {
   var openMenuButton = document.getElementById('open-menu');
   var closeMenuButton = document.getElementById('close-menu');
+  var content = document.getElementById('content');
+  var menu = document.getElementById('menu');
 
   openMenuButton.addEventListener('click', openMenu);
   closeMenuButton.addEventListener('click', closeMenu);
 
   function openMenu() {
+    hide(content);
+    show(menu);
     hide(openMenuButton);
     show(closeMenuButton);
   }
 
   function closeMenu() {
+    hide(menu);
+    show(content);
     hide(closeMenuButton);
     show(openMenuButton);
   }
