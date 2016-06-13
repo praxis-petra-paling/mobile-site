a3aceae1578e55524374dffea9d28793e00aa180
Christian Paling <christian.paling@googlemail.com>
2016-06-13 18:05:59 +0200

    changed rotation of images

--- src/js/photogallery.js
+++ src/js/photogallery.js
@@ -5,12 +5,42 @@
   var praxisItems = [
     {
       src: 'images/praxis/Erstes.jpg',
-      w: 1000,
+      w: 750,
       h: 1000
     },
     {
       src: 'images/praxis/Zweites.jpg',
       w: 1000,
+      h: 1000
+    },
+    {
+      src: 'images/praxis/Drittes.jpg',
+      w: 1000,
+      h: 750
+    },
+    {
+      src: 'images/praxis/Viertes.jpg',
+      w: 750,
+      h: 1000
+    },
+    {
+      src: 'images/praxis/Fünftes.jpg',
+      w: 750,
+      h: 1000
+    },
+    {
+      src: 'images/praxis/Sechstes.jpg',
+      w: 1000,
+      h: 666
+    },
+    {
+      src: 'images/praxis/Siebtes.jpg',
+      w: 1000,
+      h: 750
+    },
+    {
+      src: 'images/praxis/Achtes.jpg',
+      w: 750,
       h: 1000
     }
   ];
@@ -23,6 +53,6 @@
   };
 
 // Initializes and opens PhotoSwipe
-  var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+  var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, praxisItems, options);
   document.getElementById('praxis').addEventListener('click', gallery.init);
 })();
