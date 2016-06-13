318bfbc9d744d8b0ed6e526c09b81cb14cc55fc0
Christian Paling <christian.paling@googlemail.com>
2016-06-13 18:19:00 +0200

    basic first version of gallery finished

--- src/js/photogallery.js
+++ src/js/photogallery.js
@@ -1,58 +1,60 @@
 (function() {
-  var pswpElement = document.querySelectorAll('.pswp')[0];
+  function createGallery() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
 
 // build items array
-  var praxisItems = [
-    {
-      src: 'images/praxis/Erstes.jpg',
-      w: 750,
-      h: 1000
-    },
-    {
-      src: 'images/praxis/Zweites.jpg',
-      w: 1000,
-      h: 1000
-    },
-    {
-      src: 'images/praxis/Drittes.jpg',
-      w: 1000,
-      h: 750
-    },
-    {
-      src: 'images/praxis/Viertes.jpg',
-      w: 750,
-      h: 1000
-    },
-    {
-      src: 'images/praxis/Fünftes.jpg',
-      w: 750,
-      h: 1000
-    },
-    {
-      src: 'images/praxis/Sechstes.jpg',
-      w: 1000,
-      h: 666
-    },
-    {
-      src: 'images/praxis/Siebtes.jpg',
-      w: 1000,
-      h: 750
-    },
-    {
-      src: 'images/praxis/Achtes.jpg',
-      w: 750,
-      h: 1000
-    }
-  ];
+    var praxisItems = [
+      {
+        src: 'images/praxis/Erstes.jpg',
+        w: 750,
+        h: 1000
+      },
+      {
+        src: 'images/praxis/Zweites.jpg',
+        w: 1000,
+        h: 1000
+      },
+      {
+        src: 'images/praxis/Drittes.jpg',
+        w: 1000,
+        h: 750
+      },
+      {
+        src: 'images/praxis/Viertes.jpg',
+        w: 750,
+        h: 1000
+      },
+      {
+        src: 'images/praxis/Fünftes.jpg',
+        w: 750,
+        h: 1000
+      },
+      {
+        src: 'images/praxis/Sechstes.jpg',
+        w: 1000,
+        h: 666
+      },
+      {
+        src: 'images/praxis/Siebtes.jpg',
+        w: 1000,
+        h: 750
+      },
+      {
+        src: 'images/praxis/Achtes.jpg',
+        w: 750,
+        h: 1000
+      }
+    ];
 
 // define options (if needed)
-  var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-  };
+    var options = {
+      showAnimationDuration: 0
+    };
 
 // Initializes and opens PhotoSwipe
-  var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, praxisItems, options);
-  document.getElementById('praxis').addEventListener('click', gallery.init);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, praxisItems, options);
+    gallery.init();
+  }
+
+  document.getElementById('praxis').addEventListener('click', createGallery);
 })();
