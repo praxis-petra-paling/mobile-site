2cc2f5155a8df82ec123fcfdeba9d5ef8189d735
Christian Paling <christian.paling@googlemail.com>
2016-06-13 16:25:37 +0200

    Added Example photo gallery

---
+++ dist/js/photogallery.js
@@ -0,0 +1,1 @@
+'use strict';(function(){var pswpElement=document.querySelectorAll('.pswp')[0];var items=[{src:'https://placekitten.com/600/400',w:600,h:400},{src:'https://placekitten.com/1200/900',w:1200,h:900}];var options={index:0};var gallery=new PhotoSwipe(pswpElement,PhotoSwipeUI_Default,items,options);gallery.init()})();
